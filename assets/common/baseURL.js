5d636e421c0830fea884f6d871a4b99c3be3b7a2
lucasbogo <lucasbogo93@gmail.com>
2022-12-09 18:45:24 -0300

    didatic explanations

--- assets/common/baseURL.js
+++ assets/common/baseURL.js
@@ -1,17 +1,17 @@
 import { Platform } from "react-native";
 
 // Deployed to Heroku 
-// let baseURL = 'https://newmodern-server.herokuapp.com/api/v1/'
+let baseURL = 'https://newmodern-server.herokuapp.com/api/v1/'
 
 // Armazenar a URL em uma String vazia
 // let baseURL = '';
 
 
 // Verificar qual plataforma está sendo utilizada.
-{
-    Platform.OS == 'android'
-        ? baseURL = "https://1364-177-73-98-243.sa.ngrok.io/api/v1/"
-        : baseURL = "http://localhost:3000/api/v1/"
-}
+// {
+//     Platform.OS == 'android'
+//         ? baseURL = "https://1364-177-73-98-243.sa.ngrok.io/api/v1/"
+//         : baseURL = "http://localhost:3000/api/v1/"
+// }
 
 export default baseURL;
